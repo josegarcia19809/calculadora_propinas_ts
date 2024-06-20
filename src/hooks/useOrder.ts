3ac4d13f6deea5cee9179778ed0472e46eed97a6
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-20 14:38:52 -0600

    PROYECTO-Calculadora de Propinas TS: Agregando el signature a la función de agregar articulo con TypeScript

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,11 @@
 import {useState} from "react";
-import {OrderItem} from "../types";
+import {MenuItem, OrderItem} from "../types";
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]);
 
-    const addItem = () => {
-        console.log("Agregando")
+    const addItem = (item: MenuItem) => {
+        console.log("Agregando", item)
     }
     return {
         addItem
